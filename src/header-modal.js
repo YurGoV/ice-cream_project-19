f7d27f0ecb5d2162e6a8b036699c5f238b747770
Mariia Prokopenko <Klymenkomasha@gmail.com>
2022-08-11 22:36:48 +0300

    Добавляет модальное окно на кнопку Buy now

---
+++ src/header-modal.js
@@ -0,0 +1,32 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-header-modal-open]"),
+    closeModalBtn: document.querySelector("[data-header-modal-close]"),
+    modal: document.querySelector("[header-data-modal]"),
+    body: document.querySelector("[data-body]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-header-mobile-open]"),
+    modal: document.querySelector("[header-data-modal]"),
+    body: document.querySelector("[data-body]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
